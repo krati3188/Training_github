e9ddb29e0bb20b1c58fda53fc7049dccd703c170
Krati Mandloi <krati.mandloi@bounteous.com>
2024-11-27 15:45:00 +0530

    # This is a combination of 2 commits. # This is the 1st commit message:
    training: edited file
    
    # This is the commit message #2:
    
    update1
    
    update2
    
    update2
    
    update1

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hello world from krati mandloi and Aryan Rajput");
+
         System.out.println("Hello world from krati mandloi");
+        System.out.println("new edited file");
+System.out.println("updated 1");
     }
 }
